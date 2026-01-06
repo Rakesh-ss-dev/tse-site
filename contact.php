ade9d81d325a01003d3d35e2eae598f180fb24c4
rakesh <rakesh.s@skillsigma.com>
2026-01-06 12:19:31 +0530

    changes

--- contact.php
+++ contact.php
@@ -37,8 +37,8 @@
                             <div>
                                 <img src="images/call_icon.svg" class="mb-2" width="30" alt="">
                                 <h5 class="text-white">Phone:</h5>
-                                <p class="text-white">040-40203358, 040-40273355</p>
-                                <a href="tel:+91 93911 33223" class="text-white text-decoration-none">Call: +91 93911 33223</a>
+                                <p class="text-white mb-0">040-40203358, 040-40273355</p>
+                                <a href="tel:+91 93911 33223" class="text-white text-decoration-none mb-3">Call: +91 93911 33223</a>
                             </div>
                             <div>
                                 <img src="images/email_icon.svg" class="mb-2" width="30" alt="">

ea3dd52f63ea0b96811ebb4a0556930516290d42
rakesh <rakesh.s@skillsigma.com>
2026-04-08 14:31:57 +0530

    moving bootsrap to local

--- styles.php
+++ styles.php
@@ -16,16 +16,12 @@
 <link rel="stylesheet" href="css/owl.carousel.min.css" media="print" onload="this.media='all'">
 <link rel="stylesheet" href="css/owl.theme.default.min.css" media="print" onload="this.media='all'">
 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
 <link
     href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
-    rel="preload" as="style">
+    rel="stylesheet">
 <noscript>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200..700;1,200..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
-        rel="stylesheet">
 </noscript>

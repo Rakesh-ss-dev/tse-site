b678bd90bc24256f5dfd68436120533ce4e99090
rakesh <rakesh.s@skillsigma.com>
2026-04-08 11:08:25 +0530

    meta tags

--- header.php
+++ header.php
@@ -1,45 +1,5 @@
 <?php include 'dbconfig.php' ?>
-<?php
-$file = basename($_SERVER['PHP_SELF']);
-$id = isset($_GET['id']) ? $_GET['id'] : '';
 
-$sql = "SELECT * FROM `competency` WHERE `id`='" . $id . "'";
-$res = $conn->query($sql);
-print_r($res);
-// Default values
-$title = "TSE | Corporate Competency & Strategic Alliances";
-$desc = "Scalable workforce development and professional competency solutions for global enterprises.";
-$robots = "index, follow";
-
-switch ($file) {
-  case 'about.php':
-  case 'about-us.php':
-    $title = "About TSE | Professional Growth & Corporate Strategy";
-    $desc = "Discover how TSE bridges the industry skill gap with innovative professional development methodologies.";
-    break;
-  case 'methodologies.php':
-    $title = "Performance Optimization Methodologies | TSE";
-    $desc = "Explore our proprietary frameworks designed to enhance professional competency and employee performance.";
-    break;
-  case 'certifications.php':
-    $title = "Professional Credentials & Industry Validations | TSE";
-    $desc = "Standardize organizational excellence with our specialized certification programs and validated credentials.";
-    break;
-  case 'pay.php':
-    $title = "Secure Payment Portal | TSE";
-    $robots = "noindex, nofollow";
-    break;
-  case 'competency.php':
-    // You can add a database lookup here to get the real name of the competency
-    $title = "Specialized Competency Training (ID: $id) | TSE";
-    $desc = "Advance your organizational capabilities with TSE's specialized corporate competency frameworks.";
-    break;
-}
-?>
-
-<title><?php echo $title; ?></title>
-<meta name="description" content="<?php echo $desc; ?>">
-<meta name="robots" content="<?php echo $robots; ?>">
 <header>
   <nav id="header" class="navbar navbar-expand-sm navbar-dark">
     <div class="container col-md-10 col-11">

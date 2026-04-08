fa983ccc370b68230f999408b11c70ab8a6374d5
rakesh <rakesh.s@skillsigma.com>
2026-04-08 12:20:24 +0530

    optimizing lcp

--- scripts.php
+++ scripts.php
@@ -1,56 +1,59 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-    crossorigin="anonymous"></script>
-
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"
+    defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-    crossorigin="anonymous"></script>
-<script src="js/owl.carousel.min.js"></script>
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"
+    defer></script>
+<script src="js/owl.carousel.min.js" defer></script>
 <script>
-    $('.c-d-item').hover(function (e) {
-        e.preventDefault();
-        $('.c-d-item').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active')
-    })
-    tabControl();
+    document.addEventListener('DOMContentLoaded', function () {
+        // Now it's safe to use jQuery ($)
 
+        $('.c-d-item').hover(function (e) {
+            e.preventDefault();
+            $('.c-d-item').removeClass('active'); // Simplified: no .each() needed here
+            $(this).addClass('active');
+        });
 
-    var resizeTimer;
-    $(window).on('resize', function (e) {
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(function () {
-            tabControl();
-        }, 250);
-    });
+        tabControl();
 
-    function tabControl() {
-        var tabs = $('.tabbed-content').find('.tabs');
-        if (tabs.is(':visible')) {
-            tabs.find('a').on('click', function (event) {
-                event.preventDefault();
-                var target = $(this).attr('href'),
-                    tabs = $(this).parents('.tabs'),
-                    buttons = tabs.find('a'),
-                    item = tabs.parents('.tabbed-content').find('.item');
-                buttons.removeClass('active');
-                item.removeClass('active');
-                $(this).addClass('active');
-                $(target).addClass('active');
-            });
-        } else {
-            $('.item').on('click', function () {
-                var container = $(this).parents('.tabbed-content'),
-                    currId = $(this).attr('id'),
-                    items = container.find('.item');
-                container.find('.tabs a').removeClass('active');
-                items.removeClass('active');
-                $(this).addClass('active');
-                container.find('.tabs a[href$="#' + currId + '"]').addClass('active');
-            });
+        var resizeTimer;
+        $(window).on('resize', function (e) {
+            clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(function () {
+                tabControl();
+            }, 250);
+        });
+
+        function tabControl() {
+            var tabs = $('.tabbed-content').find('.tabs');
+            if (tabs.is(':visible')) {
+                // Use .off() before .on() to prevent multiple event bindings during resize
+                tabs.find('a').off('click').on('click', function (event) {
+                    event.preventDefault();
+                    var target = $(this).attr('href'),
+                        container = $(this).parents('.tabbed-content');
+
+                    container.find('.tabs a').removeClass('active');
+                    container.find('.item').removeClass('active');
+
+                    $(this).addClass('active');
+                    $(target).addClass('active');
+                });
+            } else {
+                $('.item').off('click').on('click', function () {
+                    var container = $(this).parents('.tabbed-content'),
+                        currId = $(this).attr('id');
+
+                    container.find('.tabs a').removeClass('active');
+                    container.find('.item').removeClass('active');
+
+                    $(this).addClass('active');
+                    container.find('.tabs a[href$="#' + currId + '"]').addClass('active');
+                });
+            }
         }
-    }
+    });
 </script>
--- styles.php
+++ styles.php
@@ -1,10 +1,21 @@
+<link rel="preconnect" href="https://cdn.jsdelivr.net">
+<link rel="preconnect" href="https://cdnjs.cloudflare.com">
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-<link rel="stylesheet" href="css/owl.carousel.min.css" />
-<link rel="stylesheet" href="css/owl.theme.default.min.css" />
 <link rel="stylesheet" href="css/style.css" />
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" media="print"
+    onload="this.media='all'">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" media="print"
+    onload="this.media='all'">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" media="print"
+    onload="this.media='all'">
+<link rel="stylesheet" href="css/owl.carousel.min.css" media="print" onload="this.media='all'">
+<link rel="stylesheet" href="css/owl.theme.default.min.css" media="print" onload="this.media='all'">
+
+<noscript>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+</noscript>
